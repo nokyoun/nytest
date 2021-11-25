dda013820f889950b4a12815147339227f0a1285
NY\nokyoun <nokyoun@hotmail.com>
2021-11-25 23:32:30 +0700

    static3

--- config/settings.py
+++ config/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [
     str(BASE_DIR.joinpath('static')),
 ]
 
-#STATICFILES_STORAGE = 'whitenoise.stotage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.stotage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
